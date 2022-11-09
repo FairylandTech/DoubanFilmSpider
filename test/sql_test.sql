6d4045a67c22cf5c6508aef14ceb84d237f49868
AliceEngineer <alice_engineer@yeah.net>
2022-11-10 00:02:37 +0800

    数据采集完成
    Signed-off-by: AliceEngineer <alice_engineer@yeah.net>

--- test/sql_test.sql
+++
@@ -1,2 +0,0 @@
-insert into tb_douban_movies.tb_movies_info (name, director_id, assistant_director, screenwriter, player_master_id, player, first_play, type, country, length, introduction, link)
-values ('自拍', 1, '李四,王五', '李四四,王五五', 1, '赵六,老八', '2022-09-30', '剧情,素人', '日本,新加坡', 137, '自拍', 'https://baidu.com');
